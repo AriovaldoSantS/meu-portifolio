f2e31d6da5f7b9201fb6386dfd9245f8a1d5f875
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-10-30 15:30:54 -0300

    Corrige configuração de rotas e deploy no Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
 import Contato from './pages/Contato';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
@@ -9,14 +7,12 @@ import Sobre from './pages/Sobre';
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contato" element={<Contato />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
